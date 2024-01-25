8060617bc7d9500d5a897dcf73a68593ebfbd329
BatuhanKIRICI <k.sukr48@gmail.com>
2024-01-26 01:57:28 +0300

    "HTTP Request"

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("C'mon!");
+  res.send("<h1>C'mon!</h1>");
+});
+
+app.get("/about", (req, res) => {
+  res.send("<h2>About me!</h2>");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("<h2>Call me!</h2>");
 });
 
 app.listen(port, () => {
