28a27db3dd4e20b2b0ffa6368257d4e0e6fd0a8e
BatuhanKIRICI <k.sukr48@gmail.com>
2024-01-26 11:43:44 +0300

    "Postman"

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 import express from "express";
-
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("<h1>C'mon!</h1>");
+  res.send("<h1>Home Page</h1>");
 });
 
-app.get("/about", (req, res) => {
-  res.send("<h2>About me!</h2>");
+app.post("/register", (req, res) => {
+  res.sendStatus(201);
 });
 
-app.get("/contact", (req, res) => {
-  res.send("<h2>Call me!</h2>");
+app.put("/user/angela", (req, res) => {
+  res.sendStatus(200);
+});
+
+app.patch("/user/angela", (req, res) => {
+  res.sendStatus(200);
+});
+
+app.delete("/user/angela", (req, res) => {
+  res.sendStatus(200);
 });
 
 app.listen(port, () => {
-  console.log(`Server listening ${port}!`);
+  console.log(`Server started on port ${port}`);
 });
