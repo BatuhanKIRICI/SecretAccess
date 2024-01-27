51964fd46afe2ea661e944d0485426eec9bb3a37
BatuhanKIRICI <k.sukr48@gmail.com>
2024-01-27 13:05:38 +0300

    "CleanUp"

--- index.js
+++ index.js
@@ -1,29 +1,3 @@
-import express from "express";
-import bodyParser from "body-parser";
-
-const app = express();
-const port = 3000;
-
-app.get("/", (req, res) => {
-  res.send("<h1>Home Page</h1>");
-});
-
-app.post("/register", (req, res) => {
-  res.sendStatus(201);
-});
-
-app.put("/user/angela", (req, res) => {
-  res.sendStatus(200);
-});
-
-app.patch("/user/angela", (req, res) => {
-  res.sendStatus(200);
-});
-
-app.delete("/user/angela", (req, res) => {
-  res.sendStatus(200);
-});
-
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
-});
+//To see how the final website should work, run "node solution.js".
+//Make sure you have installed all the dependencies with "npm i".
+//The password is ILoveProgramming
